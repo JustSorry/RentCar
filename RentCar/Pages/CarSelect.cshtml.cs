ecf29a1da0d23f645b29d879dcb51392492cc9c8
Andrey Laptanovich <laptanovich.andrey.04@gmail.com>
2022-10-21 15:25:22 +0300

    Deleted any pictires. Added posibility of filter car with any filters.

--- RentCar/Pages/CarSelect.cshtml.cs
+++ RentCar/Pages/CarSelect.cshtml.cs
@@ -1,27 +1,30 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using RentCar.Models;
+using System.Reflection.Metadata.Ecma335;
 
 namespace RentCar.Pages
 {
     public class CarSelectModel : PageModel
     {
         public Car[] ResCars = new Car[0];
-        public static string? CarBody;
+        public new string? Request;
 
-        public void OnGet()
+        public void OnGet(string? CarBody, string? CountryOfProd)
         {
+            Request = CarBody == "allBodies" ? CountryOfProd : (CountryOfProd == "allCountries" ? CarBody : CountryOfProd);
             using (ApplicationContext db = new ApplicationContext())
             {
                 Car[] carArray = db.Cars.ToArray();
-                ResCars = (from car in carArray where FilterCar(car, CarBody ?? "") select car).ToArray();
+                ResCars = (from car in carArray where FilterCar(car, Request ?? "") select car).ToArray();
             }
         }
 
         private bool FilterCar(Car car, string searchingCar)
         {
             bool isFinded =
-                (car.CarBody?.Contains(searchingCar) ?? false);
+                (car.CarBody?.Contains(searchingCar) ?? false) ||
+                (car.CountryOfProd?.Contains(searchingCar) ?? false);
             return isFinded;
         }
     }
